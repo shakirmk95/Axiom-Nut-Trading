1fbef93918894bc94acba70f776bf5ad2f3f5c7c
shakirmk95 <shakirmk95@gmail.com>
2017-12-03 12:25:36 +0530

    Final

--- contact_me.php
+++ contact_me.php
@@ -1,7 +1,7 @@
 <?php
 
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-	echo "No arguments Provided!";
+	echo "No arguments Provided!, Please go back to <a href='axiomnuttrading.com'>previous page</a>";
 	return false;
 }
    
@@ -10,13 +10,22 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 
-$to = 'axiomnuttrading@gmail.com';
+$to = 'info@axiomnuttrading.com';
 // $to = 'srkm95@gmail.com';
-$email_subject = "Message from Website Contact Form:  $name - $email_address";
+$email_subject = "Message from Website Contact Form:  $name";
 $email_body = "$message";
-mail($to, $email_subject, $email_body);
-echo "Mail Send Successfully";
-// return true;         
-header('Location: ' . $_SERVER['HTTP_REFERER']);
-// die();
+$mailheaders = "From: info@axiomnuttrading.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$mailheaders .= "Reply-To: $email_address";   
+// mail($to, $email_subject, $email_body, $mailheader) or die("Error!");
+if(!mail($to, $email_subject, $email_body, $mailheader)) {
+	echo "Mail Sending Not Successfully, Please go back to <a href='axiomnuttrading.com'>previous page</a>";
+	echo "<script type='text/javascript'>alert('Mail Sending Not Successfully');</script>";
+	// return false; 
+}else {
+	echo "Mail Send Successfully, Please go back to <a href='axiomnuttrading.com'>previous page</a>";
+	echo "<script type='text/javascript'>alert('Mail Send Successfully');</script>";
+	// return true; 
+}        
+header('Location: ' . $_SERVER['HTTP_REFERER']); 
+die();
 ?>
